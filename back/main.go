a6a00a7520869975241b223d799e721f511dd7b8
Leo Champion <leo.champion@protonmail.com>
2018-02-06 12:51:47 +0100

    Add: categorie in main.go

--- back/main.go
+++ back/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"./categorie"
 	"./news"
 	"./utils"
 	"github.com/gin-gonic/gin"
@@ -18,11 +19,27 @@ func testInsert(db *gorm.DB) {
 		Summary:         "News about a random movie",
 		Content:         "News content blablabla\n blablablablabalandahuyegesgfegfuegfuegfeyges",
 	}
+
 	db.Create(&news)
+	categorie1 := categorie.Categorie{
+		Name:        "Meilleure Interprétation",
+		Description: "",
+	}
+	db.Create(&categorie1)
+	categorie2 := categorie.Categorie{
+		Name:        "Meilleur Espoir",
+		Description: "",
+	}
+	db.Create(&categorie2)
+	categorie3 := categorie.Categorie{
+		Name:        "Meilleur Scénario Original",
+		Description: "",
+	}
+	db.Create(&categorie3)
 }
 
 func createSchema(db *gorm.DB) {
-	for _, model := range []interface{}{&news.News{} /*, &Edition{}, &Categorie{}, &Award{}, &Movie{}, &Person{}, &Interview{}, &Article{}, &Application{}*/} {
+	for _, model := range []interface{}{&news.News{}, &categorie.Categorie{} /*, &Edition{}, , &Award{}, &Movie{}, &Person{}, &Interview{}, &Article{}, &Application{}*/} {
 		db.DropTable(model)
 		db.AutoMigrate(model)
 	}
@@ -35,6 +52,7 @@ func main() {
 	engine := gin.Default()
 	router := engine.Group("/v1")
 	news.NewsRegister(router)
+	categorie.CategorieRegister(router)
 	engine.Run()
 	defer db.Close()
 }
