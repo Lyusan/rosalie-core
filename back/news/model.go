d6ce2960089c1c0c878ce0e5d33da229f7082262
Leo Champion <leo.champion@protonmail.com>
2018-02-06 09:27:00 +0100

    git add news/model.go
    - struct
    - FindManyNews()
    - FindNewsByID(id)

---
+++ back/news/model.go
@@ -0,0 +1,33 @@
+package news
+
+import (
+	"time"
+
+	"../utils"
+
+	"github.com/jinzhu/gorm"
+)
+
+type News struct {
+	gorm.Model
+	Title           string
+	Author          string
+	PublicationDate time.Time
+	UpdateDate      time.Time
+	Summary         string
+	Content         string
+}
+
+func FindManyNews() ([]News, error) {
+	var news []News
+	db := utils.GetDB()
+	err := db.Order("publication_date").Find(&news).Error
+	return news, err
+}
+
+func FindNewsByID(id int) (News, error) {
+	var news News
+	db := utils.GetDB()
+	err := db.First(&news, id).Error
+	return news, err
+}
