4b033190e8b74f5f423e9a9b6ceb4da167201d4b
Leo Champion <leo.champion@protonmail.com>
2018-02-13 16:56:28 +0100

    Add question model

---
+++ question/model.go
@@ -0,0 +1,31 @@
+package question
+
+import (
+	"time"
+
+	"../utils"
+	"github.com/jinzhu/gorm"
+)
+
+type Question struct {
+	gorm.Model
+	FirstName   string
+	LastName    string
+	BirthDate   time.Time
+	Description string
+	ImgUrl      string
+}
+
+func FindManyQuestions() ([]Question, error) {
+	var questions []Question
+	db := utils.GetDB()
+	err := db.Find(&questions).Error
+	return questions, err
+}
+
+func FindQuestionByID(id int) (Question, error) {
+	var question Question
+	db := utils.GetDB()
+	err := db.First(&question, id).Error
+	return question, err
+}
