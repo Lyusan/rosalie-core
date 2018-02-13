a317e55c22705d16bc8f6e7d2ad2a5cf1c801c99
Leo Champion <leo.champion@protonmail.com>
2018-02-13 14:47:18 +0100

    Add award model

---
+++ award/model.go
@@ -0,0 +1,29 @@
+package award
+
+import (
+	"../utils"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Award struct {
+	gorm.Model
+	Categorie   Categorie
+	CategorieID int
+	Movie       Movie
+	MovieID     int
+}
+
+func FindManyAwards() ([]Award, error) {
+	var awards []Award
+	db := utils.GetDB()
+	err := db.Find(&awards).Error
+	return awards, err
+}
+
+func FindAwardByID(id int) (Award, error) {
+	var award Award
+	db := utils.GetDB()
+	err := db.First(&award, id).Error
+	return award, err
+}
