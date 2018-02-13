be2c81d2030780d27e509785df104544aa773a32
Leo Champion <leo.champion@protonmail.com>
2018-02-13 16:54:42 +0100

    Add application model

---
+++ application/model.go
@@ -0,0 +1,28 @@
+package application
+
+import (
+	"../utils"
+	"github.com/jinzhu/gorm"
+)
+
+type Application struct {
+	gorm.Model
+	Award   Award
+	AwardID int
+	Movie   Move
+	MoveID  int
+}
+
+func FindManyApplications() ([]Application, error) {
+	var applications []Application
+	db := utils.GetDB()
+	err := db.Find(&applications).Error
+	return applications, err
+}
+
+func FindApplicationByID(id int) (Application, error) {
+	var application Application
+	db := utils.GetDB()
+	err := db.First(&application, id).Error
+	return application, err
+}
