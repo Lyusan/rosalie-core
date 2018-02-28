8d99bdd7154b885a5bc8a548a586da490619d844
Leo Champion <leo.champion@protonmail.com>
2018-02-28 08:32:37 +0100

    add award serializer

--- serializer/award.go
+++ serializer/award.go
@@ -1,1 +1,52 @@
 package serializer
+
+import (
+	"../model"
+	"github.com/gin-gonic/gin"
+)
+
+type Award struct {
+	ID            uint          `json:"id"`
+	CategorieName string        `json:"categorie_name"`
+	CategorieDesc string        `json:"categorie_desc"`
+	Candidates    []Application `json:"candidates"`
+	Nominees      []Application `json:"nominees"`
+	Winner        []Application `json:"winner"`
+}
+
+type AwardSerializer struct {
+	C *gin.Context
+	model.Award
+}
+
+func (s *AwardSerializer) Response() Award {
+	candidatesSerializer := ApplicationsSerializer{s.C, s.Applications}
+	candidates := candidatesSerializer.Response()
+	nomineesSerializer := ApplicationsSerializer{s.C, s.Applications}
+	nominees := nomineesSerializer.Response()
+	winnerSerializer := ApplicationsSerializer{s.C, s.Applications}
+	winner := winnerSerializer.Response()
+	response := Award{
+		ID:            s.ID,
+		CategorieName: s.Categorie.Name,
+		CategorieDesc: s.Categorie.Description,
+		Candidates:    candidates,
+		Nominees:      nominees,
+		Winner:        winner,
+	}
+	return response
+}
+
+type AwardsSerializer struct {
+	C      *gin.Context
+	Awards []model.Award
+}
+
+func (s *AwardsSerializer) Response() []Award {
+	response := []Award{}
+	for _, award := range s.Awards {
+		serializer := AwardSerializer{s.C, award}
+		response = append(response, serializer.Response())
+	}
+	return response
+}
