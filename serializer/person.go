8d275523b8af446778e1ae96dde19b369ed8549d
Leo Champion <leo.champion@protonmail.com>
2018-02-28 08:32:18 +0100

    add person serializer

--- serializer/person.go
+++ serializer/person.go
@@ -1,1 +1,46 @@
 package serializer
+
+import (
+	"../model"
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	ID          uint   `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	BirthDate   string `json:"birth_date"`
+	Description string `json:"description"`
+	ImgUrl      string `json:"image_url"`
+}
+
+type PersonSerializer struct {
+	C *gin.Context
+	model.Person
+}
+
+func (s *PersonSerializer) Response() Person {
+	response := Person{
+		ID:          s.ID,
+		FirstName:   s.FirstName,
+		LastName:    s.LastName,
+		BirthDate:   s.BirthDate.UTC().Format("2006-01-02T15:04:05.999Z"),
+		Description: s.Description,
+		ImgUrl:      s.ImgUrl,
+	}
+	return response
+}
+
+type PeopleSerializer struct {
+	C      *gin.Context
+	People []model.Person
+}
+
+func (s *PeopleSerializer) Response() []Person {
+	response := []Person{}
+	for _, person := range s.People {
+		serializer := PersonSerializer{s.C, person}
+		response = append(response, serializer.Response())
+	}
+	return response
+}
