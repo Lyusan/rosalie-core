ad0ad0cc228bcad2180270fe564560728c499505
Leo Champion <leo.champion@protonmail.com>
2018-03-01 11:38:26 +0100

    Fix admin use initDB to avoid nil pointer

--- admin/admin.go
+++ admin/admin.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"fmt"
 
 	"../model"
 	"../utils"
@@ -13,15 +15,56 @@ import (
 )
 
 func main() {
-	admin := admin.New(&admin.AdminConfig{DB: utils.DB})
-	slice := model.GetModels()
-	for _, model := range slice {
-		admin.AddResource(model)
+	db, err := utils.InitDB()
+	if err != nil {
+		log.Fatalf("DB: Cannot connect: %s\n", err)
 	}
+	Admin := admin.New(&admin.AdminConfig{DB: db})
+	// slice := model.GetModels()
+	// for _, model := range slice {
+	// 	admin.AddResource(model)
+	// }
+	adminApplication := Admin.AddResource(&model.Application{})
+	Admin.AddResource(&model.Movie{})
+	Admin.AddResource(&model.Person{})
+	adminApplication.NewAttrs("VotesNominees", "VotesWinner", "MovieID", "PersonID")
+	adminApplication.Meta(&admin.Meta{Name: "MovieID", Label: "Movie", Type: "select_one",
+		Config: &admin.SelectOneConfig{
+		  Collection: func(_ interface{}, context *admin.Context) (options [][]string) {
+			var movies []model.Movie
+			context.GetDB().Find(&movies)
+	
+			for _, n := range movies {
+			  idStr := fmt.Sprintf("%d", n.ID)
+			  var option = []string{idStr, n.Title}
+			  options = append(options, option)
+			}
+	
+			return options
+		  },
+		},
+	  })
+	  adminApplication.Meta(&admin.Meta{Name: "PersonID", Label: "Person", Type: "select_one",
+		Config: &admin.SelectOneConfig{
+		  Collection: func(_ interface{}, context *admin.Context) (options [][]string) {
+			var people []model.Person
+			context.GetDB().Find(&people)
+	
+			for _, n := range people {
+			  idStr := fmt.Sprintf("%d", n.ID)
+			  var option = []string{idStr, n.FirstName, n.LastName}
+			  options = append(options, option)
+			}
+	
+			return options
+		  },
+		},
+	  })
+
 	// initalize an HTTP request multiplexer
 	mux := http.NewServeMux()
 
 	// Mount admin interface to mux
-	admin.MountTo("/admin", mux)
+	Admin.MountTo("/admin", mux)
 	http.ListenAndServe(":9000", mux)
 }
