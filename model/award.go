a0b6654f0454b4481db013d15a7d273e60dff623
Leo Champion <leo.champion@protonmail.com>
2018-03-01 16:19:55 +0100

    Model award add fct to list: apps nominees winner

--- model/award.go
+++ model/award.go
@@ -3,6 +3,7 @@ package model
 import (
 	"../utils"
 
+	"github.com/bradfitz/slice"
 	"github.com/jinzhu/gorm"
 )
 
@@ -28,6 +29,51 @@ func FindAwardByID(id uint) (Award, error) {
 	db := utils.GetDB()
 	err := db.First(&award, id).Error
 	return award, err
+}
+
+func (s *Award) FindRelatedCandidates() []Application {
+	var applications []Application
+	db := utils.GetDB()
+	db.Find(s).Related(&applications)
+	return applications
+}
+
+func (s *Award) FindRelatedNominees() []Application {
+	var applications []Application
+	var nominees []Application
+	db := utils.GetDB()
+	db.Find(s).Related(&applications)
+	if len(applications) > 0 {
+		// TODO use ORDER BY SQL
+		slice.Sort(applications[:], func(i, j int) bool {
+			return applications[i].VotesNominees > applications[j].VotesNominees
+		})
+		index := 0
+		currentVoteNomineesValue := applications[index].VotesNominees
+		for ; index < len(applications) && (currentVoteNomineesValue == applications[index].VotesNominees || index < s.NumberNominees); index++ {
+			nominees = append(nominees, applications[index])
+		}
+	}
+	return nominees
+}
+
+func (s *Award) FindRelatedWinner() []Application {
+	var applications []Application
+	var winner []Application
+	db := utils.GetDB()
+	db.Find(s).Related(&applications)
+	if len(applications) > 0 {
+		// TODO use ORDER BY SQL
+		slice.Sort(applications[:], func(i, j int) bool {
+			return applications[i].VotesWinner > applications[j].VotesWinner
+		})
+		index := 0
+		currentVoteNomineesValue := applications[index].VotesWinner
+		for ; index < len(applications) && currentVoteNomineesValue == applications[index].VotesWinner; index++ {
+			winner = append(winner, applications[index])
+		}
+	}
+	return winner
 }
 
 func (s *Award) FindRelatedCategorie() Categorie {
